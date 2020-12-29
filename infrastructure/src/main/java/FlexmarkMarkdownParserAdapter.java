9ac60a5ea3e13bafbf61197d2b530e37ee235efa
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-29 17:08:35 +0100

    refactor parser adapter

--- infrastructure/src/main/java/FlexmarkMarkdownParserAdapter.java
+++ infrastructure/src/main/java/FlexmarkMarkdownParserAdapter.java
@@ -1,12 +1,12 @@
 import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
 import fr.baldir.joplin.ports.in.MarkdownParserPort;
-
-import javax.swing.text.html.HTMLDocument;
 
 public class FlexmarkMarkdownParserAdapter implements MarkdownParserPort {
     @Override
     public String parse(String markdownContent) {
-        var document = com.vladsch.flexmark.parser.Parser.builder().build().parse(markdownContent);
+        final var parser = Parser.builder().build();
+        var document = parser.parse(markdownContent);
         var renderer = HtmlRenderer.builder().build();
         return renderer.render(document);
     }
